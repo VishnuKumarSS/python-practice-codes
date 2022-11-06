c2923045c5b001c6370b352ccea280605fa3005e
VishnuKumarSS <vishnukumarss941@gmail.com>
2022-11-07 01:04:59 +0530

    Pushed multiplication table.py

---
+++ multiplication table.py
@@ -0,0 +1,13 @@
+a = 1
+b = int(input("Enter a number : "))
+c = int(input("Enter how many times it should be multiplied : "))
+d = 0
+while (a<=c):
+    d = a*b
+    print(d)
+    a+=1
+#for i in range(1,c+1):
+   #i*=b
+   #print(i)    
+
+ 
